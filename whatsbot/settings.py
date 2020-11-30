5e0c851e82ca3d1a7f5aa879704b058ef21d6e8d
edumats <eduardo.mats@gmail.com>
2020-11-30 11:41:25 -0300

    debugging

--- whatsbot/settings.py
+++ whatsbot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'protected-lowlands-90139.herokuapp.com']
 
