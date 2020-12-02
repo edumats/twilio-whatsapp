e3dd7fc1845646a454d2e34ca136be3a3c4977bb
edumats <eduardo.mats@gmail.com>
2020-12-02 14:32:36 -0300

    Changed static storage setting

--- whatsbot/settings.py
+++ whatsbot/settings.py
@@ -176,7 +176,7 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whatsbot.storage.WhiteNoiseStaticFilesStorage'
 
 # Path for login/Logout
 
