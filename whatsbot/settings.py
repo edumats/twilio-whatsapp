9869e9f108df93b18599e11230a8ac5a9c921201
edumats <eduardo.mats@gmail.com>
2020-12-02 00:12:50 -0300

    debugging

--- whatsbot/settings.py
+++ whatsbot/settings.py
@@ -170,10 +170,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
